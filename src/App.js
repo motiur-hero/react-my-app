4d532139161ba0fed4773676cf20d227b89f3f9a
motiur-hero <motiurgreenpower@gmail.com>
2020-05-03 16:58:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // var style={color:'red',
+  // backgroundColor:'yellow'
+  // }
+  // var person={
+  //   name: "Dr.Mahfuz",
+  //   job: "Doctor" 
+  // }
+  // var person2={
+  //   name: "eva rahman",
+  //   job: "kokil kondhi"
+  // }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <p>My first similar component</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+
+
       </header>
     </div>
   );
 }
+
+function Person() {
+  const personStyle={
+    border:"2px solid red",
+    margin:'15px',
+  }
+  return (<div style={personStyle}>
+          <h1>shakib al hasan</h1>
+          <h3>Hero of the Year</h3>
+    </div>)
+}
+  
 
 export default App;
